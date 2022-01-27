a85918d95861f6355895c5141987c965b340dc68
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-01-27 22:44:47 +1300

    updated client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,16 +9,26 @@ import (
 func main() {
 	var strs []crazystrings.CrazyString
 
-	cs1, _ := crazystrings.NewCrazyString("composition")
-	cs2, _ := crazystrings.NewBigCrazyString("composition")
+	cs1, _ := crazystrings.NewCrazyString("encapsulation")
+	cs2, _ := crazystrings.NewBigCrazyString("encapsulation")
 
-	cs3, _ := crazystrings.NewCrazyString("polymorphism")
-	cs4, _ := crazystrings.NewBigCrazyString("polymorphism")
+	cs3, _ := crazystrings.NewCrazyString("abstraction")
+	cs4, _ := crazystrings.NewBigCrazyString("abstraction")
+
+	cs5, _ := crazystrings.NewCrazyString("polymorphism")
+	cs6, _ := crazystrings.NewBigCrazyString("polymorphism")
+
+	cs7, _ := crazystrings.NewCrazyString("composition")
+	cs8, _ := crazystrings.NewBigCrazyString("composition")
 
 	strs = append(strs, cs1)
 	strs = append(strs, cs2)
 	strs = append(strs, cs3)
 	strs = append(strs, cs4)
+	strs = append(strs, cs5)
+	strs = append(strs, cs6)
+	strs = append(strs, cs7)
+	strs = append(strs, cs8)
 
 	for _, str := range strs {
 		fmt.Println(str.Scramble())
